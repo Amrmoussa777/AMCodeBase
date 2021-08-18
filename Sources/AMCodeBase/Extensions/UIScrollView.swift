c2b824ecf411ed50870b9080bc0188a3b49e3280
Amr moussa <amrmoussa2016@icloud.com>
2021-08-18 02:31:51 +0200

    Updated UISCrollView Extension

---
+++ Sources/AMCodeBase/Extensions/UIScrollView.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Amr Moussa on 18/08/2021.
+//
+
+import UIKit
+
+public extension UIScrollView{
+    
+     func scrollToTop(){
+        let topoffset = CGPoint(x: 0, y: 0)
+        setContentOffset(topoffset, animated: true)
+    }
+    
+     func scrollToBottom(){
+        let bottomOffset = CGPoint(x: 0, y: contentSize.height - bounds.height + contentInset.bottom)
+        setContentOffset(bottomOffset, animated: true)
+    }
+    
+}
