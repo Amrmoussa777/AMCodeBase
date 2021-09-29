3b024ea2773696c4df99809cd6884656f00a363c
Amr moussa <amrmoussa2016@icloud.com>
2021-09-29 14:09:59 +0200

    show Toast Added

--- Sources/AMCodeBase/Extensions/UIVC+Ext.swift
+++ Sources/AMCodeBase/Extensions/UIVC+Ext.swift
@@ -74,7 +74,33 @@ public extension UIViewController{
 //        present(registerVC, animated: true)
     }
    
-   
+    func showToast(message : String, font: UIFont? = UIFont.systemFont(ofSize: 19)
+                   ,icon:UIImage? = Images.checkMarkImage) {
+            let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 100, y: self.view.frame.size.height-100, width: 250, height: 40))
+            toastLabel.roundShape()
+            toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+            toastLabel.textColor = UIColor.white
+            toastLabel.font = font
+            toastLabel.textAlignment = .center
+            
+            let fullString = NSMutableAttributedString(string: " ")
+            let imgAttachment = NSTextAttachment()
+            imgAttachment.bounds = CGRect(x: -5, y: -3 , width: 20, height: 20)
+            imgAttachment.image = icon
+            let image1String = NSAttributedString(attachment: imgAttachment)
+            fullString.append(image1String)
+            fullString.append(NSAttributedString(string: "  " +  message))
+
+            toastLabel.attributedText = fullString
+            toastLabel.alpha = 1.0
+            
+            self.view.addSubview(toastLabel)
+            UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+                 toastLabel.alpha = 0.0
+            }, completion: {(isCompleted) in
+                toastLabel.removeFromSuperview()
+            })
+        }
     
     
 
