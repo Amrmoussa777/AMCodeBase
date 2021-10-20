274d147f400dfb397b72a5ca13e970407baf54e2
Amr moussa <amrmoussa2016@icloud.com>
2021-10-20 09:39:49 +0200

    update network manager

--- Sources/AMCodeBase/Managers/AMNetworkManager.swift
+++ Sources/AMCodeBase/Managers/AMNetworkManager.swift
@@ -40,7 +40,7 @@ open class AMNetworkManagerBase :loginStatusProtocol{
             
             
             guard  let image = UIImage(data: data) else {return}
-            self.cashe[urlKey]  = image
+//            self.cashe[urlKey]  = image
             completion(image)
         }
         
