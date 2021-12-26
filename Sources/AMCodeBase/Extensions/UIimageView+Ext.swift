63fabb1e4fd0f9c6df76f79f6e16e27f2ab3da49
Amr moussa <amrmoussa2016@icloud.com>
2021-12-26 11:40:30 +0200

    add downlaodImagewWithUIimageReturn

--- Sources/AMCodeBase/Extensions/UIimageView+Ext.swift
+++ Sources/AMCodeBase/Extensions/UIimageView+Ext.swift
@@ -11,29 +11,43 @@ import UIKit
 public extension UIImageView {
     
     func downloadImage(fromURL url: String) {
-         AMNetworkManager.Shared.downloadImage(from: url) { [weak self] (image) in
-         guard let self = self else { return }
-         DispatchQueue.main.async {
-            self.image = image
-            self.contentMode = .scaleAspectFill
-         }
+        AMNetworkManager.Shared.downloadImage(from: url) { [weak self] (image) in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.image = image
+                self.contentMode = .scaleAspectFill
+            }
             
-     }
-       
+        }
+        
     }
     
     func downloadImageWithAnimation(fromURL url: String,withColor:UIColor) {
         let loading = showImageLoadingLoadingView(color: withColor)
-         AMNetworkManager.Shared.downloadImage(from: url) { [weak self] (image) in
-         guard let self = self else { return }
-         DispatchQueue.main.async {
-            loading.removeFromSuperview()
-            self.image = image
-            self.contentMode = .scaleAspectFill
-         }
-     }
-       
+        AMNetworkManager.Shared.downloadImage(from: url) { [weak self] (image) in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                loading.removeFromSuperview()
+                self.image = image
+                self.contentMode = .scaleAspectFill
+            }
+        }
+        
     }
-}
     
+    
+    func downlaodImagewWithUIimageReturn(fromURL url: String, completed:@escaping (UIImage?)->()) {
+        AMNetworkManager.Shared.downloadImage(from: url) { [weak self] (image) in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.image = image
+                self.contentMode = .scaleAspectFill
+                completed(image)
+            }
+        }
+        
+    }
+    
+}
+
 
