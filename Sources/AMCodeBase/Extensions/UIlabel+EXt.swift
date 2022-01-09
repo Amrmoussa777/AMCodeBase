dfd16ee0fc32799541c475a55f6c3129bf95c102
Amr moussa <amrmoussa2016@icloud.com>
2022-01-09 10:41:38 +0200

    add new attrText in uilabel

--- Sources/AMCodeBase/Extensions/UIlabel+EXt.swift
+++ Sources/AMCodeBase/Extensions/UIlabel+EXt.swift
@@ -44,5 +44,27 @@ public extension UILabel {
         textColor = .black
         return dotString
     }
+    
+    
+    func  setMBSettingAtrributedTextInTheEndOfText(fString:String,lString:String,IMG:UIImage)->NSAttributedString{
+        let attachment = NSTextAttachment()
+        attachment.image = IMG.withTintColor(.systemGreen)
+        attachment.bounds = CGRect(x: 0, y: 0, width: 30, height: 17)
+        let attachmentStr = NSAttributedString(attachment: attachment)
+        
+        font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        let attrs = [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 20)]
+        
+        let attributedString = NSMutableAttributedString(string:fString)
+        let dotString = NSMutableAttributedString(string: "", attributes:attrs)
+        let boldString = NSMutableAttributedString(string: lString, attributes:attrs)
+        
+       
+        dotString.append(attributedString)
+        dotString.append(boldString)
+        dotString.append(attachmentStr)
+        textColor = .black
+        return dotString
+    }
 
 }
