c9ce66242e8ad8ce8ee9aede9a9c9054a28c0471
Amr Moussa <a.moussa@wecodeforyou.io>
2022-06-02 16:20:09 +0200

    add UIKit preview methods in UIView and UIViewCntroller extension

--- Sources/AMCodeBase/Extensions/UIVC+Ext.swift
+++ Sources/AMCodeBase/Extensions/UIVC+Ext.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import SwiftUI
 
 public extension UIViewController{
     
+        
     
   
     
@@ -154,6 +156,26 @@ public extension UIViewController{
         dismiss(animated: false)
     }
     
+    
+    // enable preview for UIKit
+    @available(iOS 13, *)
+    private struct Preview: UIViewControllerRepresentable {
+        // this variable is used for injecting the current view controller
+        let viewController: UIViewController
+        
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            //
+        }
+    }
+    
+    @available(iOS 13, *)
+    func showPreview() -> some View {
+        Preview(viewController: self)
+    }
 
     
 }
--- Sources/AMCodeBase/Extensions/UIView+Ext.swift
+++ Sources/AMCodeBase/Extensions/UIView+Ext.swift
@@ -415,6 +415,26 @@ public extension UIView {
             return circleArray
         }
         
+    // enable preview for UIKit
+        // source: https://dev.to/gualtierofr/preview-uikit-views-in-xcode-3543
+        @available(iOS 13, *)
+        private struct Preview: UIViewRepresentable {
+            typealias UIViewType = UIView
+            let view: UIView
+            func makeUIView(context: Context) -> UIView {
+                return view
+            }
+            
+            func updateUIView(_ uiView: UIView, context: Context) {
+                //
+            }
+        }
+        
+        @available(iOS 13, *)
+        func showPreview() -> some View {
+            // inject self (the current UIView) for the preview
+            Preview(view: self)
+        }
     
 
 }
